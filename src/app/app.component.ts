862ebb3d06bfe997f16a612dac8541aa9a265a03
aadarsh <aadarshg2562003@example.com>
2025-03-05 21:36:22 +0530

    [Aadarsh] Add . Implemented one-way data binding using interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HelloWorld';
+  title: string = "Hello from BridgeLabz"; // Property for interpolation
 }
+
