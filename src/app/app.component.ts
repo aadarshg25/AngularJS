f715821dee1896863430928008b470681367b2f6
aadarsh <aadarshg2562003@example.com>
2025-03-05 22:21:16 +0530

    [Aadarsh] Add . Implemented Event Binding to open BridgeLabz website on logo click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title: string = 'Helloworld';
-  imgUrl: string = 'assets/logo.jpg';
+  title: string = 'Hello from BridgeLabz.';
+  imgUrl: string = 'assets/logo.jpg'; 
+  url: string = 'https://www.bridgelabz.com'; 
 
   ngOnInit(): void {
     this.title = 'Hello from BridgeLabz.';
   }
+
+  onClick(event: Event): void {
+    console.log('BridgeLabz logo clicked!', event);
+    window.open(this.url, '_blank'); 
+  }
 }
+
 
